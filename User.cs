ab0d76a2cfd0cd24a1f9ed8e26c4578555cdb075
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-12-19 21:42:03 +0100

    Add project files.

---
+++ User.cs
@@ -0,0 +1,10 @@
+﻿namespace jwtWebApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+         
+    }
+}
