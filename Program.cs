6d48532a7cda3f63f4fbb90514720b0897b3af84
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-01-03 01:05:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
