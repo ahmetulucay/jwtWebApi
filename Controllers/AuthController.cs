5701f43274aa44a998874b9e09bdf03c103c804e
Ahmet Ulucay <57094137+ahmetulucay@users.noreply.github.com>
2024-03-19 13:36:42 +0100

    Update AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -14,45 +14,33 @@ namespace jwtWebApi.Controllers
         public static User user = new User();
 
         private readonly IConfiguration _configuration;
-        public AuthController(IConfiguration configuration)
-        {
-            _configuration= configuration;
-        }
+        public AuthController(IConfiguration configuration){
+            _configuration= configuration;}
 
         [HttpPost("register")]
-        public async Task<ActionResult<User>> Register(UserDto request)
-        {
+        public async Task<ActionResult<User>> Register(UserDto request){
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.Username = request.Username;
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            return Ok(user);
-        }
+            return Ok(user);}
 
         [HttpPost("login")]
-        public async Task<ActionResult<string>> Login (UserDto request)
-        {
-            if (user.Username != request.Username)
-            {
-                return BadRequest("User not found.");
-            }
-            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
-            {
-                return BadRequest("Wrong Password");
-            }
+        public async Task<ActionResult<string>> Login (UserDto request){
+            if (user.Username != request.Username){
+                return BadRequest("User not found.");}
+                
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt)){
+                return BadRequest("Wrong Password");}
 
             string token = CreateToken(user);
-            return Ok(token);
-        }
+            return Ok(token);}
 
-        private string CreateToken(User user)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, user.Username)
-            };
+        private string CreateToken(User user){
+            List<Claim> claims = new List<Claim>{
+                new Claim(ClaimTypes.Name, user.Username)};
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings: Token").Value));
 
@@ -65,28 +53,16 @@ namespace jwtWebApi.Controllers
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return jwt;
+            return jwt;}
 
-        }
-
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
-        {
-            using (var hmac = new HMACSHA512())
-            {
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) {
+            using (var hmac = new HMACSHA512()){
                 passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-        }
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));}}
 
-        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-        {
-            using(var hmac = new HMACSHA512(passwordSalt))
-            {
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt){
+            using(var hmac = new HMACSHA512(passwordSalt)){
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                return computedHash.SequenceEqual(passwordHash);
-            }
-        }
-
-
+                return computedHash.SequenceEqual(passwordHash);}}
     }
 }
